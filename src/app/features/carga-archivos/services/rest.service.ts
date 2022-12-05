b643af7a0557c6b2d8bd967ddf8491c40d5d1d13
SantisanR2 <sa.ramirezr@uniandes.edu.co>
2022-12-05 18:48:27 -0500

    Ultimo

--- src/app/features/carga-archivos/services/rest.service.ts
+++ src/app/features/carga-archivos/services/rest.service.ts
@@ -1,27 +1,34 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class RestService {
     
-  // API url
-  baseApiUrl = "https://file.io"
-    
   constructor(private http:HttpClient) { }
-  
-  // Returns an observable
+
   upload(file:any):Observable<any> {
-  
+
       // Create form data
-      const formData = new FormData(); 
-        
+      const formData = new FormData();
+          
       // Store form name as "file" with file data
       formData.append("file", file, file.name);
+
+      var headers = new HttpHeaders().set('content-type', 'application/octet-stream');
+      var headers1 = new HttpHeaders().set('X-RapidAPI-Key', '4868a7784emsh1526ff8e8f7fc54p11a703jsn8030bde13037');
+      var headers2 = new HttpHeaders().set('X-RapidAPI-Host', 'pastin1.p.rapidapi.com');
+      var headers3 = new HttpHeaders().set('useQueryString', 'true');
+
+      const options = {
+        "body": file,
+        "headers": {'content-type':'application/octet-stream','X-RapidAPI-Key':'4868a7784emsh1526ff8e8f7fc54p11a703jsn8030bde13037','X-RapidAPI-Host':'pastin1.p.rapidapi.com','useQueryString':'true'},
+      };
         
       // Make http post request over api
       // with formData as req
-      return this.http.post(this.baseApiUrl, formData)
+      
+      return this.http.request('POST',"https://pastin1.p.rapidapi.com/upload", options );
   }
 }
